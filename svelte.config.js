245c28614a976e5f35efe09fd118f7d2103a1bb8
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-05-03 11:06:53 +0200

    initial setup

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte'],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess()],
+
+	vitePlugin: {
+		inspector: true
+	},
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+export default config;
---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { beaches } from './src/beaches'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		typography,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'custom',
+						enhancements: true,
+					},
+				],
+				custom: [
+					beaches,
+				],
+			},
+		}),
+	],
+} satisfies Config;
