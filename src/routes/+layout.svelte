898aba50180934706f007853dc6f46dd209b8a74
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-05-06 18:06:17 +0200

    add iconset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import '../app.postcss';
+	import '@fortawesome/fontawesome-free/css/solid.min.css';
+	import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import SidebarLayout from '$lib/SidebarLayout.svelte';
+	import { base } from '$app/paths';
 </script>
 
 <SidebarLayout>
 	<svelte:fragment slot="sidebar">
-		<h1 class="text-4xl font-bold">Welcome to Skeleton</h1>
+		<h1><a href={base}>The Beach Atlas</a></h1>
+		<p>A Literary Atlas of the Beach in the Long 20th Century</p>
 	</svelte:fragment>
 	<a
 		class="btn bg-quarternary-500 text-surface-700 hover:bg-surface-700 hover:text-quarternary-500"
@@ -14,4 +16,5 @@
 	>
 		Launch Documentation
 	</a>
+	<i class="fa-solid fa-user"></i>
 </SidebarLayout>
