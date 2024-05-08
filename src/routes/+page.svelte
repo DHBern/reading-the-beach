59b4029a58f71d19d19a613cbab600ebf02b968f
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-05-08 18:22:39 +0200

    finish home mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	//@ts-ignore
 	import dimensions from '$lib/Worldmap-Base.png?as=meta:height;width';
 	import { getModalStore } from '@skeletonlabs/skeleton';
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
 	import type { ModalSettings } from '@skeletonlabs/skeleton';
 
 	const modalStore = getModalStore();
@@ -43,16 +44,16 @@
 		<section class="mt-6">
 			<h2 class="h2 my-4">explore by region</h2>
 			<ul>
-				<li>
+				<li class="my-2">
 					<a href="{base}/region/black-atlantic" class="btn variant-filled-secondary"
 						>Black Atlantic</a
 					>
 				</li>
-				<li>
+				<li class="my-2">
 					<a href="{base}/region/mediterranean" class="btn variant-filled-tertiary">Mediterranean</a
 					>
 				</li>
-				<li>
+				<li class="my-2">
 					<a
 						href="{base}/region/northern-sea"
 						class="btn bg-quarternary-500 text-surface-700 hover:bg-surface-700 hover:text-quarternary-500"
@@ -65,14 +66,46 @@
 		<section class="mt-6">
 			<h2 class="h2 my-4">explore by topic</h2>
 			<ul>
-				<li><a href="{base}/topic/death" class="btn variant-ringed">death</a></li>
-				<li><a href="{base}/topic/leisure" class="btn variant-ringed">leisure</a></li>
-				<li><a href="{base}/topic/migration" class="btn variant-ringed">migration and exile</a></li>
-				<li><a href="{base}/topic/myth" class="btn variant-ringed">myth</a></li>
-				<li><a href="{base}/topic/pollution" class="btn variant-ringed">pollution</a></li>
+				<li class="my-2"><a href="{base}/topic/death" class="btn variant-ringed">death</a></li>
+				<li class="my-2"><a href="{base}/topic/leisure" class="btn variant-ringed">leisure</a></li>
+				<li class="my-2">
+					<a href="{base}/topic/migration" class="btn variant-ringed">migration and exile</a>
+				</li>
+				<li class="my-2"><a href="{base}/topic/myth" class="btn variant-ringed">myth</a></li>
+				<li class="my-2">
+					<a href="{base}/topic/pollution" class="btn variant-ringed">pollution</a>
+				</li>
 			</ul>
 		</section>
-		<button class="btn variant-filled mt-6">About this project</button>
+		<Accordion class="mt-6 fill-primary-500">
+			<AccordionItem>
+				<svelte:fragment slot="summary">About this project</svelte:fragment>
+				<svelte:fragment slot="content">
+					<p>
+						In narrative, the beach functions as a site of heightened intensity and crisis. In many
+						Anglophone novels and stories from c. 1890 onward, it is the setting of the turning
+						point and climax, notably creative acts, scenes of self-discovery and rebirth, erotic
+						encounters and scenes of death.
+					</p>
+					<p>
+						These literary representations reflect the importance of the beach in everyday culture,
+						as a vacation site, a contact zone, a site of capitalist production and, in recent
+						years, an arrival zone for refugees and a scene of terrorist attacks. With the
+						irrefutability of global warming, the littoral ecotone has also emerged as a site where
+						its effects have become most dramatically visible.
+					</p>
+					<p>
+						Despite its literary, cultural and ecological importance, however, the beach has played
+						a relatively minor role in scholarship until fairly recently. This project seeks to
+						redress this neglect, reclaiming the beach as a central space of modernity and
+						postmodernity. We will approach the beach from the perspectives of postcolonial studies,
+						ecocriticism and new materialism. One of the aims of this project is to produce an
+						interactive online literary atlas of beach narratives, making our research accessible to
+						a wider public.
+					</p>
+				</svelte:fragment>
+			</AccordionItem>
+		</Accordion>
 	</svelte:fragment>
 	<div class="grid">
 		<div class="row-span-full col-span-full">
