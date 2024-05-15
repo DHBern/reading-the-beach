f6a78f2196e6382fc7fd651c193782dbda435509
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-05-15 15:43:10 +0200

    add buttons on map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,8 +29,10 @@
 		`,
 		buttonTextCancel: 'Close'
 	};
+	let svg: SVGElement;
 </script>
 
+<svelte:window on:resize={() => (svg = svg)} />
 <SidebarLayout>
 	<svelte:fragment slot="sidebar">
 		<p>A Literary Atlas of the Beach in the Long 20th Century</p>
@@ -45,9 +47,9 @@
 			<h2 class="h2 my-4">explore by region</h2>
 			<ul>
 				<li class="my-2">
-					<a href="{base}/region/black-atlantic" class="btn variant-filled-secondary"
-						>Black Atlantic</a
-					>
+					<a href="{base}/region/black-atlantic" class="btn variant-filled-secondary">
+						Black Atlantic
+					</a>
 				</li>
 				<li class="my-2">
 					<a href="{base}/region/mediterranean" class="btn variant-filled-tertiary">Mediterranean</a
@@ -107,9 +109,15 @@
 			</AccordionItem>
 		</Accordion>
 	</svelte:fragment>
-	<div class="grid">
+	<div
+		class="grid"
+		style="--scaling: {Math.max(
+			svg?.clientWidth / dimensions.width,
+			svg?.clientHeight / dimensions.height
+		) || 1};"
+	>
 		<div class="row-span-full col-span-full h-screen">
-			<enhanced:img src={MapImage} class="h-full object-cover" />
+			<enhanced:img src={MapImage} class="h-full object-cover" alt="world map" />
 		</div>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -117,25 +125,53 @@
 			preserveAspectRatio="xMidYMid slice"
 			id="map"
 			class="row-span-full col-span-full h-screen w-full"
+			bind:this={svg}
 		>
 			<a href="{base}/region/black-atlantic">
 				<polygon
 					fill="transparent"
 					points="962,1957 1096,2075 1356,2173 1596,2248 2399,2465 2482,2343,2415,2197,2332,2110,2151,2099,2100,2004,2155,1886,2163,1819,2139,1772,2147,1713,2171,1669,2226,1650,2234,1595,2210,1579,1545,1654,1340,1642,1269,1646,1194,1756,1037,1788,989,1862"
-				></polygon></a
-			>
+				></polygon>
+				<foreignObject x="1600" y="1900" width="800" height="500">
+					<a
+						href="{base}/region/black-atlantic"
+						class="btn variant-filled-secondary origin-top-left"
+						style="transform: scale(calc(1 / var(--scaling)));"
+					>
+						Black Atlantic
+					</a>
+				</foreignObject>
+			</a>
 			<a href="{base}/region/mediterranean">
 				<polygon
 					fill="transparent"
 					points="2159,1819,2277,1807,2442,1874,2549,1874,2734,1819,2734,1717,2647,1669,2568,1610,2521,1614,2482,1539,2332,1536,2230,1591,2230,1646,2171,1669,2143,1713,2135,1768"
-				></polygon></a
-			>
+				></polygon>
+				<foreignObject x="2300" y="1650" width="800" height="500">
+					<a
+						href="{base}/region/mediterranean"
+						class="btn variant-filled-tertiary origin-top-left"
+						style="transform: scale(calc(1 / var(--scaling)));"
+					>
+						Mediterranean
+					</a>
+				</foreignObject>
+			</a>
 			<a href="{base}/region/northern-sea">
 				<polygon
 					fill="transparent"
 					points="1269,1646,1541,1658,2206,1579,2332,1536,2403,1484,2553,1433,2639,1307,2631,1252,2702,1260,2927,1075,2895,984,2655,425,2356,461,1911,528,1860,843,1828,980,1694,1110,1659,1181,1615,1063,1548,1075,1450,1410,1320,1469"
-				></polygon></a
-			>
+				></polygon>
+				<foreignObject x="1800" y="1400" width="800" height="500">
+					<a
+						href="{base}/region/northern-sea"
+						class="btn bg-quarternary-500 text-surface-700 hover:bg-surface-700 hover:text-quarternary-500 origin-top-left"
+						style="transform: scale(calc(1 / var(--scaling)));"
+					>
+						Northern Sea
+					</a>
+				</foreignObject>
+			</a>
 		</svg>
 	</div>
 </SidebarLayout>
