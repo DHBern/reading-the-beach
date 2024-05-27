c6b780a7bc41d21e7579a32f439450552b5b0a83
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-05-27 18:38:36 +0200

    fix sidebar shadow

--- src/lib/SidebarLayout.svelte
+++ src/lib/SidebarLayout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="max-w-full lg:grid lg:grid-cols-[320px_1fr]">
-	<aside class="px-4 lg:h-screen shadow-[0_.5rem_1rem_rgba(0,0,0,.5)] flex flex-col">
+	<aside class="px-4 lg:h-screen shadow-[0_.5rem_1rem_rgba(0,0,0,.5)] flex flex-col z-10">
 		<h1 class="flex-none text-3xl font-extrabold my-[0.6rem]">
 			<a href="{base}/">The Beach Atlas</a>
 		</h1>
