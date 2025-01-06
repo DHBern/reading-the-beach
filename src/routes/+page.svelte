97f7a7656e779ac291b6f49afbf1819f34f9e941
flicksolutions <sebastian@flick.solutions>
2025-01-06 19:00:32 +0100

    update about content (and prose-styles)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,26 +12,19 @@
 
 	const info: ModalSettings = {
 		type: 'alert',
-		body: `
-		<p>Welcome to Digital Shores: An Interactive Atlas of Beach Narratives! Here you can access our visualisation of the function of the beach in (mainly) anglophone literature, published between 1890 and 2023. A map displays our selection of texts – novels, short stories, and narrative nonfiction – in which the beach plays a crucial role. From each text we have chosen relevant excerpts that showcase the function of the beach.</p>
-		<p>The atlas offers different filters that allow users to approach our material from several angles:</p>
+		body: `<div class="prose dark:prose-inverted">
+		<p>This atlas was created as a part of our SNSF project on The Beach in the Long Twentieth Century (<a class="anchor" href="https://data.snf.ch/grants/grant/192810" target="_blank">100012_192810 / 1</a>). It links the four individual projects that the team members worked on. The fact that it emerged from these different projects results in the following biases:</p>
 		<ul class="list-disc list-outside ml-4">
-			<li>A temporal slider shows historical developments</li>
-			<li>Texts can be grouped according to categories such as agents, events, genre, materiality, and mood</li>
-			<li>Regional maps illustrate the specificities of three model regions</li>
-			<li>Thematic maps visualise the geographical distribution of five overarching themes we have identified</li>
+			<li>Since two of our sub-projects explicitly concentrated on the Northern Seas and the Mediterranean respectively, there is an uneven distribution of texts per region. This results in fewer texts set in the Black Atlantic.</li>
+			<li>While we aimed to offer a representative sample of texts from the period of the long twentieth century, despite the fact that three of our sub-projects focus on the second half of the century, the temporal distribution of texts varies. This might partly also be due to the fluctuating popularity of the beach as a literary setting across the twentieth century.</li>
+			<li>Only one sub-project focuses on nonfictional texts, which results in an overrepresentation of these texts in the Northern Seas region.</li>
+			<li>The project mainly explored anglophone literature, but individual sub-projects also have a comparative perspective, which is reflected in our corpus.</li>
 		</ul>
-		<p>Enter here and embark on a journey to the beaches of the literary world!</p>
-		<h2 class="h3 my-3">Explore by region</h2>
-        <p>The texts are grouped into three main regions, colour-coordinated, which can be more closely examined: <a href="${base}/region/black-atlantic" class="badge variant-filled-secondary hover:!text-secondary-500 hover:!bg-surface-900">Black Atlantic</a>,
-		<a href="${base}/region/mediterranean"  class="badge variant-filled-tertiary hover:!text-tertiary-500 hover:!bg-surface-900">Mediterranean</a>,
-		<a href="${base}/region/northern-sea" class="badge bg-quarternary-500 text-surface-900  hover:!text-quarternary-500 hover:!bg-surface-900">Northern Seas</a>. While some settings can be exactly identified, literature often plays with (re-)imagined and fictionalised places. Therefore, texts can only be placed on the map as approximations without claim to geographical precision.</p>
-      <h2 class="h3 my-3">Explore by theme</h2>
-        <p>Five maps show the distribution of texts according to themes we have identified as particularly prevalent in relation to the beach: <a href="${base}/theme/death" class="badge bg-surface-900 hover:bg-primary-500 hover:text-surface-900 border">death</a>,
-		 <a href="${base}/theme/leisure" class="badge bg-surface-900 hover:bg-primary-500 hover:text-surface-900 border">leisure</a>,
-		 <a href="${base}/theme/migration" class="badge bg-surface-900 hover:bg-primary-500 hover:text-surface-900 border">migration and exile</a>,
-		 <a href="${base}/theme/myth" class="badge bg-surface-900 hover:bg-primary-500 hover:text-surface-900 border">myth</a> and
-		 <a href="${base}/theme/pollution" class="badge bg-surface-900 hover:bg-primary-500 hover:text-surface-900 border">pollution</a>. These have crystallised as key concerns shared across texts in our readings.</p>
+		<p><i>Guðrun í Jákupsstovu</i> is a PhD candidate in the project. Her dissertation examines contemporary British and Scandinavian nature writing texts that portray “encounters with time” in littoral spaces, such as beaches, coasts, and islands and explores how these enable temporal experiences and understandings in relation to climate change. For the atlas, she explores how pollution and climate change are reflected in the coastal environments of her texts, as well as the role of myth in tandem with these topics.</p>
+		<p><i>Marion Troxler</i> is a PhD candidate in the project. Her thesis explores how reading the beach together with merfolk, given their shared hybrid and metamorphic qualities, provides insights into their potential for resisting hegemonic dualistic hierarchies. Her selection of texts highlights the changing role of mermaids, selkies, Mami Wata, and other related figures over the long 20th century, especially in regard to patriarchal and (post-)colonial structures.</p>
+		<p><i>Ursula Kluwick</i> is Senior Researcher in the project. She specialises in representations of water and the beach, and her book <a class="anchor" href="https://www.upress.virginia.edu/title/5983/" target="_blank">Haunting Ecologies: Victorian Water Writing</a> is just out with the University of Virginia Press (2024). Her current book project deals with the representation of the Mediterranean beach since the 1950s, and her selection of texts for the atlas shows the wealth of themes associated with this region, particularly leisure, migration and exile, and myth.</p>
+		<p><i>Virginia Richter</i> is the PI of the project. She has a longstanding interest in the beach and together with Ursula Kluwick, she organised an early conference (2009) and edited <a class="anchor" href="https://www.routledge.com/The-Beach-in-Anglophone-Literatures-and-Cultures-Reading-Littoral-Space/Kluwick-Richter/p/book/9780367880965?srsltid=AfmBOor8_ZkeUDKWBj5TuPKOV29ApfgHfKtIDk84K5oTYxVf3mbRHXC-" target="_blank">The Beach in Anglophone Literatures and Cultures</a> (Ashgate 2015), one of the first collections on this topic. Her current project approaches the beach in modernist literature from a comparative perspective, and she is also writing a field guide to littoral studies. For the atlas, Virginia selected texts from the first half of the long twentieth century.</p>
+		</div>
 		`,
 		buttonTextCancel: 'Close'
 	};
@@ -294,7 +287,7 @@
 			bind:clientHeight={svgHeight}
 			bind:clientWidth={svgWidth}
 		>
-			<a href="{base}/region/black-atlantic" aria-label="Black Atlantic">
+			<a class="anchor" href="{base}/region/black-atlantic" aria-label="Black Atlantic">
 				<polygon
 					fill="transparent"
 					points="1272,2193,1398,2165,1556,2236,2107,2347,2308,2413,2335,2543,2398,2646,2461,2717,2552,2717,2619,2654,2650,2591,2501,2508,2501,2406,2485,2323,2450,2236,2414,2165,2359,2098,2233,2083,2142,2087,2127,2008,2186,1886,2174,1819,2142,1780,2131,1721,2209,1638,2229,1595,1776,1622,1493,1650,1339,1618,1260,1634,1217,1693,1189,1748,1087,1776,1004,1811,969,1874,949,1953,1004,2035,1091,2079"
@@ -309,7 +302,7 @@
 						Black Atlantic
 					</button>{/if}
 			</foreignObject>
-			<a href="{base}/region/mediterranean" aria-label="Mediterranean">
+			<a class="anchor" href="{base}/region/mediterranean" aria-label="Mediterranean">
 				<polygon
 					fill="transparent"
 					points="2159,1819,2277,1807,2442,1874,2549,1874,2734,1819,2734,1717,2647,1669,2568,1610,2521,1614,2482,1539,2332,1536,2230,1591,2230,1646,2171,1669,2143,1713,2135,1768"
@@ -324,7 +317,7 @@
 						Mediterranean
 					</button>{/if}
 			</foreignObject>
-			<a href="{base}/region/northern-sea" aria-label="Northern Seas">
+			<a class="anchor" href="{base}/region/northern-sea" aria-label="Northern Seas">
 				<polygon
 					fill="transparent"
 					points="1269,1646,1541,1658,2206,1579,2332,1536,2403,1484,2553,1433,2639,1307,2631,1252,2702,1260,2927,1075,2895,984,2655,425,2356,461,1911,528,1860,843,1828,980,1694,1110,1659,1181,1615,1063,1548,1075,1450,1410,1320,1469"
