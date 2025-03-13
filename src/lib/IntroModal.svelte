61af8d12eb36a36f2ad0606c3f4cdf76ec9fa5d6
flicksolutions <sebastian@flick.solutions>
2025-03-13 15:57:53 +0100

    add tab on intro modal

--- src/lib/IntroModal.svelte
+++ src/lib/IntroModal.svelte
@@ -12,34 +12,36 @@
 >
 	<header class="modal-header text-2xl font-bold">Digital Shores</header>
 	<article class="modal-body overflow-hidden prose">
-		<p>
-			Welcome to Digital Shores: An Interactive Atlas of Beach Narratives! Here you can access our
-			visualisation of the function of the beach in (mainly) anglophone literature, published
-			between 1890 and 2023. A map displays our selection of texts – novels, short stories, and
-			narrative nonfiction – in which the beach plays a crucial role. From each text we have chosen
-			relevant excerpts that showcase the function of the beach.
-		</p>
-		<p>
-			The atlas offers different filters that allow users to approach our material from several
-			angles:
-		</p>
-		<ul class="!list-disc !list-outside ml-4">
-			<li>a temporal slider shows historical developments</li>
-			<li>
-				texts can be grouped according to categories such as agents, events, genre, materiality, and
-				mood
-			</li>
-			<li>regional maps illustrate the specificities of three model regions</li>
-			<li>
-				thematic maps visualise the geographical distribution of five overarching themes we have
-				identified
-			</li>
-		</ul>
 		<TabGroup border="border-b border-primary-500">
-			<Tab bind:group={tabSet} name="region" value={0}>Explore by region</Tab>
-			<Tab bind:group={tabSet} name="theme" value={1}>Explore by theme</Tab>
+			<Tab bind:group={tabSet} name="region" value={0}>Welcome</Tab>
+			<Tab bind:group={tabSet} name="region" value={1}>Explore by region</Tab>
+			<Tab bind:group={tabSet} name="theme" value={2}>Explore by theme</Tab>
 			<svelte:fragment slot="panel">
 				{#if tabSet === 0}
+					<p>
+						Welcome to Digital Shores: An Interactive Atlas of Beach Narratives! Here you can access
+						our visualisation of the function of the beach in (mainly) anglophone literature,
+						published between 1890 and 2023. A map displays our selection of texts – novels, short
+						stories, and narrative nonfiction – in which the beach plays a crucial role. From each
+						text we have chosen relevant excerpts that showcase the function of the beach.
+					</p>
+					<p>
+						The atlas offers different filters that allow users to approach our material from
+						several angles:
+					</p>
+					<ul class="!list-disc !list-outside ml-4">
+						<li>a temporal slider shows historical developments</li>
+						<li>
+							texts can be grouped according to categories such as agents, events, genre,
+							materiality, and mood
+						</li>
+						<li>regional maps illustrate the specificities of three model regions</li>
+						<li>
+							thematic maps visualise the geographical distribution of five overarching themes we
+							have identified
+						</li>
+					</ul>
+				{:else if tabSet === 1}
 					<p>
 						The texts are grouped into three main regions, colour-coordinated, which can be more
 						closely examined: Northern Seas, Mediterranean, the Black Atlantic. While some settings
@@ -47,7 +49,7 @@
 						places. Therefore, texts can only be placed on the map as approximations without claim
 						to geographical precision.
 					</p>
-				{:else if tabSet === 1}
+				{:else if tabSet === 2}
 					<p>
 						Five maps show the distribution of texts according to themes we have identified as
 						particularly prevalent in relation to the beach: Death, Leisure, Migration and Exile,
